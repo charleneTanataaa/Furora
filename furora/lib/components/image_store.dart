86b4dbed7ed23934a98b42b2b3e08558eb85b41e
chelsyCy <chelsytioyan@gmail.com>
2026-04-23 15:50:27 +0700

    feat: saved locally feature

--- furora/lib/components/image_store.dart
+++ furora/lib/components/image_store.dart
@@ -1,19 +1,71 @@
+import 'dart:io';
 import 'dart:typed_data';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ImageStore {
-  static List<CapturedImage> images = [];
-}
-
-class CapturedImage{
+class CapturedImage {
   final Uint8List bytes;
-  final String expression;
   final DateTime date;
+  final String expression;
   final String location;
 
   CapturedImage({
     required this.bytes,
-    required this.expression,
     required this.date,
+    required this.expression,
     required this.location,
   });
+}
+
+class ImageStore {
+  static List<CapturedImage> images = [];
+
+  // Call this once at app startup in main()
+  static Future<void> loadFromDisk() async {
+    final prefs = await SharedPreferences.getInstance();
+    final dir = await getApplicationDocumentsDirectory();
+    final keys = prefs.getStringList('image_keys') ?? [];
+
+    images = [];
+    for (final key in keys) {
+      final path = '${dir.path}/$key.jpg';
+      final file = File(path);
+      if (!await file.exists()) continue;
+
+      final bytes = await file.readAsBytes();
+      final expression = prefs.getString('${key}_expression') ?? '';
+      final location = prefs.getString('${key}_location') ?? '';
+      final dateMs = prefs.getInt('${key}_date') ?? 0;
+
+      images.add(CapturedImage(
+        bytes: bytes,
+        date: DateTime.fromMillisecondsSinceEpoch(dateMs),
+        expression: expression,
+        location: location,
+      ));
+    }
+  }
+
+  // Call this when saving a new photo
+  static Future<void> addImage(CapturedImage image) async {
+    images.add(image);
+
+    final prefs = await SharedPreferences.getInstance();
+    final dir = await getApplicationDocumentsDirectory();
+    final key = 'img_${DateTime.now().millisecondsSinceEpoch}';
+
+    // Save image file
+    final file = File('${dir.path}/$key.jpg');
+    await file.writeAsBytes(image.bytes);
+
+    // Save metadata
+    await prefs.setString('${key}_expression', image.expression);
+    await prefs.setString('${key}_location', image.location);
+    await prefs.setInt('${key}_date', image.date.millisecondsSinceEpoch);
+
+    // Update key list
+    final keys = prefs.getStringList('image_keys') ?? [];
+    keys.add(key);
+    await prefs.setStringList('image_keys', keys);
+  }
 }
--- furora/lib/main.dart
+++ furora/lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:furora/components/main_navigation.dart';
+import 'package:furora/components/image_store.dart';
 
 List<CameraDescription> cameras = [];
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  cameras = await availableCameras();
+  
+  try {
+    cameras = await availableCameras();
+  } catch (e) {
+    debugPrint('Camera not supported on this platform: $e');
+  }
+  
+  await ImageStore.loadFromDisk();
   runApp(const MyApp());
 }
 
